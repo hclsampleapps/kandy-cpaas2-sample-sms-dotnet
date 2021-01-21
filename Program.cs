89a407c6fa8af92081fcc29bdf03604b412d11a6
amgupta <amgupta@rbbn.com>
2021-01-22 00:28:12 +0530

    SMS server side SDK

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using dotenv.net;
+using Nancy.Hosting.Self;
+using System;
+using System.IO;
+
+namespace sms {
+  class Program {
+    static void Main(string[] args) {
+      var PORT = 8000;
+      var uri = $"http://localhost:{PORT}";
+
+      using (var host = new NancyHost(new Uri(uri))) {
+        host.Start();
+        var isUnixFS = Path.GetPathRoot(Directory.GetCurrentDirectory()) == @"/";
+        var upPath = isUnixFS ? @"../../" : @"..\..\";
+        string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), upPath));
+        string dotenvpath = isUnixFS ? $"{path}/.env" : $"{path}\\.env";
+        DotEnv.Config(true, dotenvpath);
+        Console.WriteLine("NancyFX Stand alone test application.");
+        Console.WriteLine($"Listening on {uri}");
+        Console.WriteLine("Press enter to exit the application");
+        Console.ReadLine();
+      }
+    }
+  }
+}
